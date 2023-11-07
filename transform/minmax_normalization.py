dd1b3ba711b20761f33a9c54315115199f6f64b5
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-07 15:17:31 -0500

    correct edge_attr rounding and globalize norm-stats

--- transform/minmax_normalization.py
+++ transform/minmax_normalization.py
@@ -3,7 +3,6 @@ import os
 import torch
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 from torch_geometric.data import Data
 
 input_dir = '/content/drive/MyDrive/protein-DATA/prot-sample'
@@ -56,12 +55,11 @@ scaling_parameters_df = pd.DataFrame({
 # Save the scaling parameters to a CSV file
 scaling_parameters_df.to_csv(stats, index=False)
 
-# Initialize DataFrame for storing normalized and rounded min and max
-norm_rounded_stats = {
-    'feature': [],
-    'rounded_min': [],
-    'rounded_max': []
-}
+# Initialize lists to accumulate min and max values for the second pass
+accumulated_x_mins = []
+accumulated_x_maxs = []
+accumulated_edge_attr_mins = []
+accumulated_edge_attr_maxs = []
 
 # Second pass to scale the data using the global min and max
 for file_name in os.listdir(input_dir):
@@ -83,23 +81,29 @@ for file_name in os.listdir(input_dir):
         # Round the normalized values to four decimal places
         data_object.x = torch.round(data_object.x * 10000) / 10000
         data_object.edge_attr = torch.round(data_object.edge_attr * 10000) / 10000
-        
-        # Assuming atom_coords is a tensor in data_object, round it as well
-        if hasattr(data_object, 'atom_coords'):
-            data_object.atom_coords = torch.round(data_object.atom_coords * 10000) / 10000
-        
-        # Update the dictionary with the scaled and rounded Data object
-        data_dict[data_key] = data_object
 
-        # Update normalized and rounded stats
-        norm_rounded_stats['feature'] += feature_names_x + feature_names_edge_attr
-        norm_rounded_stats['rounded_min'] += data_object.x.min(dim=0).values.tolist() + data_object.edge_attr.min(dim=0).values.tolist()
-        norm_rounded_stats['rounded_max'] += data_object.x.max(dim=0).values.tolist() + data_object.edge_attr.max(dim=0).values.tolist()
-
+        # Accumulate min and max values for x and edge_attr
+        accumulated_x_mins.append(data_object.x.min(dim=0).values)
+        accumulated_x_maxs.append(data_object.x.max(dim=0).values)
+        accumulated_edge_attr_mins.append(data_object.edge_attr.min(dim=0).values)
+        accumulated_edge_attr_maxs.append(data_object.edge_attr.max(dim=0).values)
+        
         # Save the modified data dictionary
         output_path = os.path.join(output_dir, file_name)
         torch.save(data_dict, output_path)
 
-# Convert normalized and rounded stats to DataFrame and save
-norm_rounded_stats_df = pd.DataFrame(norm_rounded_stats)
+# Calculate the global rounded min and max for x and edge_attr features
+global_rounded_x_min = torch.stack(accumulated_x_mins).min(dim=0).values.numpy()
+global_rounded_x_max = torch.stack(accumulated_x_maxs).max(dim=0).values.numpy()
+global_rounded_edge_attr_min = torch.stack(accumulated_edge_attr_mins).min(dim=0).values.numpy()
+global_rounded_edge_attr_max = torch.stack(accumulated_edge_attr_maxs).max(dim=0).values.numpy()
+
+# Create a DataFrame for the global rounded min and max
+norm_rounded_stats_df = pd.DataFrame({
+    'feature': feature_names_x + feature_names_edge_attr,
+    'rounded_min': np.round(np.concatenate((global_rounded_x_min, global_rounded_edge_attr_min)), 4),
+    'rounded_max': np.round(np.concatenate((global_rounded_x_max, global_rounded_edge_attr_max)), 4)
+})
+
+# Save the global rounded min and max to a CSV file
 norm_rounded_stats_df.to_csv(norm_stats, index=False)
