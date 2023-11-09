f1fa367dbd65ff7cc123a43e14ec2a636e30644b
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-09 15:32:39 -0500

    concat soap to x

--- transform/atomic_posenc.py
+++ transform/atomic_posenc.py
@@ -29,29 +29,25 @@ def soap_local(input_directory, output_directory):
             atom_coords = data['atom_coords']
             num_atoms = len(atom_coords)
 
-            # This will hold the local SOAP descriptors
-            local_descriptors = []
-
             # Initialize the SOAP descriptor
             soap = SOAP(species=["H"], periodic=False, r_cut=2.5, n_max=4, l_max=4, sigma=0.1)
 
             # Create an ASE Atoms object
             system = Atoms(numbers=np.ones(num_atoms), positions=atom_coords.numpy())
 
-            # Calculate SOAP descriptor for each atom
+            # Calculate SOAP descriptor for each atom and append it to x
             for i in range(num_atoms):
                 # Calculate the descriptor for the atom
                 descriptor = soap.create(system, centers=[i])
-                local_descriptors.append(descriptor[0])
-
-            # Add the local descriptors to the data object and save
-            local_descriptors_array = np.array(local_descriptors)
-            data['local_soap'] = torch.tensor(local_descriptors_array, dtype=torch.float32)
+                # Append the descriptor to the 'x' tensor of the data
+                data['x'] = torch.cat((data['x'], torch.tensor(descriptor, dtype=torch.float32)), dim=1)
+                
+            # Save the updated data object
             data_dict[data_key] = data
             output_file_path = os.path.join(output_directory, filename)
             torch.save(data_dict, output_file_path)
 
-    print(f"All local SOAP descriptors calculated and saved to {output_directory}.")
+    print(f"All local SOAP descriptors calculated and appended to 'x' and saved to {output_directory}.")
 
 # Call the function with the input and output directories
 soap_local(input_directory, output_directory)
