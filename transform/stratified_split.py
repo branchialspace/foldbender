4cf839a4aaebaefb572e8799ebae99451ce85780
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-13 12:52:28 -0500

    Create stratified_split.py

---
+++ transform/stratified_split.py
@@ -0,0 +1,46 @@
+# Multi-label stratified split for train, val, test sets
+import os
+import torch
+from collections import Counter
+from sklearn.model_selection import train_test_split
+
+input_directory = '/path/to/input/directory'
+indices_file_path = os.path.join(input_directory, 'dataset_indices.pt')
+
+# Load all files in the input directory
+file_list = [os.path.join(input_directory, f) for f in os.listdir(input_directory)]
+
+# Extract labels for splitting purposes
+label_representations = []
+for file in file_list:
+    data = torch.load(file)
+    labels = [key for key in data['labels'].keys() if key.startswith("GO:")]
+    label_representations.append(tuple(sorted(labels)))
+
+# Function to determine if stratification is possible
+def can_stratify(labels):
+    label_counts = Counter(labels)
+    return all(count > 1 for count in label_counts.values())
+
+# If we can stratify based on the labels, we do. Otherwise, we split randomly.
+split_args = dict(test_size=0.2, random_state=42)
+if can_stratify(label_representations):
+    split_args["stratify"] = label_representations
+
+train_indices, temp_test_indices, _, _ = train_test_split(range(len(file_list)), label_representations, **split_args)
+
+temp_label_representations = [label_representations[i] for i in temp_test_indices]
+split_args_val_test = dict(test_size=0.5, random_state=42)
+if can_stratify(temp_label_representations):
+    split_args_val_test["stratify"] = temp_label_representations
+
+val_indices, test_indices = train_test_split(temp_test_indices, **split_args_val_test)
+
+# Save indices to a .pt file
+indices_dict = {
+    'train': train_indices,
+    'val': val_indices,
+    'test': test_indices
+}
+
+torch.save(indices_dict, indices_file_path)
