084ab6ec3fe1aca02c826cf478a05520b5e148b0
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-14 14:29:27 -0500

    Update alphafold_dataset.py

--- transform/alphafold_dataset.py
+++ transform/alphafold_dataset.py
@@ -1,16 +1,31 @@
+from typing import List
 import os
+import os.path as osp
 import re
 import torch
-from torch_geometric.data import Dataset
+from torch_geometric.data import Dataset, Data
 
-class AlphafoldDataset(Dataset):
-    def __init__(self, root, transform=None, pre_transform=None):
-        super(AlphafoldDataset, self).__init__(root, transform, pre_transform)
-        self.data_files = sorted(
-            [f for f in os.listdir(root) if f.endswith('.pt')],
+class Alphafold(Dataset):
+    def __init__(self, root: str):
+        self.root = root
+        self.split_file = '/content/drive/MyDrive/protein-DATA/dataset-indices.pt'
+        super().__init__(root)
+
+    @property
+    def processed_file_names(self) -> List[str]:
+        return sorted(
+            [f for f in os.listdir(self.root) if f.endswith('.pt')],
             key=lambda x: int(re.findall(r'\d+', x)[0])
         )
-        self.split_dict = torch.load(os.path.join(root, 'split_dict.pt'))
-        
+
+    def __len__(self):
+        return len(self.processed_file_names)
+
+    def __getitem__(self, idx):
+        data_path = osp.join(self.root, self.processed_file_names[idx])
+        data = torch.load(data_path)
+        return data
+
     def get_idx_split(self):
-        return self.split_dict
+        split_dict = torch.load(self.split_file)
+        return split_dict
