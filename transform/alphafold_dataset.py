67cd0e5d830018a145602bcb86d3c61ce95950cf
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-15 11:33:41 -0500

    Update alphafold_dataset.py

--- transform/alphafold_dataset.py
+++ transform/alphafold_dataset.py
@@ -6,7 +6,7 @@ import torch
 from torch_geometric.data import Dataset, Data
 
 class Alphafold(Dataset):
-    def __init__(self, root: str):
+    def __init__(self):
         self.root = '/content/drive/MyDrive/protein-DATA/sample-final'
         self.split_file = '/content/drive/MyDrive/protein-DATA/dataset-indices.pt'
 
