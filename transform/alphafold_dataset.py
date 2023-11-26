33ac21b4f54842c266cfb26cbf42c9b2da7d4717
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-11-26 12:49:20 -0500

    remove split path assignment from dataset

--- transform/alphafold_dataset.py
+++ transform/alphafold_dataset.py
@@ -1,15 +1,14 @@
 from typing import List
 import os
 import os.path as osp
-import re
 import torch
-from torch_geometric.data import Dataset, Data
+from torch_geometric.data import Dataset
+import re
 
 class Alphafold(Dataset):
     def __init__(self):
         super().__init__()
-        self.root = '/content/drive/MyDrive/protein-DATA/sample-final'
-        self.split_file = '/content/drive/MyDrive/protein-DATA/dataset-indices.pt'
+        self.root = '/content/drive/MyDrive/protein-DATA/sample_final'
 
     @property
     def processed_file_names(self) -> List[str]:
@@ -24,9 +23,5 @@ class Alphafold(Dataset):
     def get(self, idx):
         data_path = osp.join(self.root, self.processed_file_names[idx])
         data = torch.load(data_path)
-        data.filename = osp.splitext(self.processed_file_names[idx])[0]
-        return data
 
-    def get_idx_split(self):
-        split_dict = torch.load(self.split_file)
-        return split_dict
+        return data
