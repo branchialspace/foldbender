f44792f3f2bbe90e5835ce67c4f47f9c07d40a0d
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2023-12-18 15:02:16 -0500

    modify input directory instead of creating output directory

--- utils/delete_y.py
+++ utils/delete_y.py
@@ -3,19 +3,15 @@ import os
 import torch
 from torch_geometric.data import Data
 
-def delete_y(input_dir, output_dir):
-    os.makedirs(output_directory, exist_ok=True)
-    for filename in os.listdir(input_dir):
+def delete_y(directory):
+    for filename in os.listdir(directory):
         if filename.endswith('.pt'):
-            file_path = os.path.join(input_dir, filename)
+            file_path = os.path.join(directory, filename)
             data = torch.load(file_path)
             if hasattr(data, 'y'):
                 delattr(data, 'y')
-            output_file_path = os.path.join(output_dir, filename)
-            torch.save(data, output_file_path)
+            torch.save(data, file_path)
 
 if __name__ == "__main__":
-
-    input_directory = '/content/drive/MyDrive/protein-DATA/41k_sample_processed_GO'
-    output_directory = '/content/drive/MyDrive/protein-DATA/41k_sample_processed_noY'
-    delete_y(input_directory, output_directory)
+    directory = '/content/41k_sample_processed_GO'
+    delete_y(directory)
