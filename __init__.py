fe0be49e748e9db040d27f0145c71c900b1dea4b
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2024-01-10 17:39:31 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@ from .transform.ALPHA_NX import alpha_nx
 from .transform.NX_PyG import nx_pyg
 from .transform.GO_labels import go_labels
 from .transform.ESM2_labels import esm2_labels
+from .transform.foldseek_labels import foldseek_labels
 from .transform.alphafold_dataset import Alphafold
 from .transform.atomic_posenc import soap_local
 from .transform.eigen_posenc import precompute_eigens
 from .transform.minmax_norm import minmax_norm
 from .transform.stratified_split import stratified_split
+from .transform.foldseek_targets import foldseek_scored_clusters
 from .utils.delete_y import delete_y
