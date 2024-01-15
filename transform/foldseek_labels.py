62cef4897d51eeb8b2d324ed593ce79fe8cfe040
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2024-01-15 18:01:52 -0500

    add empty y for the entryid which do not belong to any clusters

--- transform/foldseek_labels.py
+++ transform/foldseek_labels.py
@@ -22,27 +22,26 @@ def foldseek_labels(input_directory, foldseek_labels):
     for filename in all_files:
         if filename.endswith('.pt'):
             entry_id = filename.split('.')[0]  # Assuming the file names are just EntryID.pt
+            file_path = os.path.join(input_directory, filename)
+            data_obj = torch.load(file_path)
 
-            # If the filename (without .pt) exists in the df's EntryID column
+            # Initialize y vector for all unique terms
+            y = torch.zeros(len(unique_terms), dtype=torch.float32)
+
             if entry_id in df['EntryID'].values:
-                # Load the .pt file from the local directory as a PyG data object
-                file_path = os.path.join(input_directory, filename)
-                data_obj = torch.load(file_path)
-
                 # Create a vector for the terms associated with this entry
-                y = torch.zeros(len(unique_terms), dtype=torch.float32)
                 entry_df = df[df['EntryID'] == entry_id]
                 for _, row in entry_df.iterrows():
                     index = term_to_index[row['Term']]
                     y[index] = row['Value']
-                data_obj.y = y
-
-                # Save the modified data object back to the same location
-                torch.save(data_obj, file_path)
+            # If the entry_id is not in foldseek_labels, y remains a zero vector
+            data_obj.y = y
+            # Save the modified data object back to the same location
+            torch.save(data_obj, file_path)
               
 if __name__ == "__main__":
     
     input_directory = "/content/drive/MyDrive/protein-DATA/sample"
-    train_terms = "/content/drive/MyDrive/cafa-5-protein-function-prediction/Train/train_terms.tsv"
+    foldseek_labels = ".tsv"
     
     foldseek_labels(input_directory, foldseek_labels)
