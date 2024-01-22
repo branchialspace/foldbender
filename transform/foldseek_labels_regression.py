46e7136b9f40c1e59e29ab4a2d58c541bbeb9b80
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2024-01-22 14:56:47 -0500

    use float16, tqdm, rename

--- transform/foldseek_labels_regression.py
+++ transform/foldseek_labels_regression.py
@@ -1,10 +1,12 @@
-# Foldseek Labels
+# Foldseek Multitarget Regression Labels
 import torch
 import pandas as pd
 import os
 from torch_geometric.data import Data
+import tqdm
 
-def foldseek_labels(input_directory, foldseek_labels):
+
+def foldseek_labels_regression(input_directory, foldseek_labels):
     # Load the foldseek_labels.tsv file and parse it
     df = pd.read_csv(foldseek_labels, sep='\t', header=None, names=['Term', 'EntryID_with_extra', 'Value'])
     df['EntryID'] = df['EntryID_with_extra'].apply(lambda x: x.split('.')[0])
@@ -18,15 +20,15 @@ def foldseek_labels(input_directory, foldseek_labels):
     unique_terms = df[df['EntryID'].isin(entries_in_directory)]['Term'].unique()
     term_to_index = {term: i for i, term in enumerate(unique_terms)}
 
-    # Process each file in the directory
-    for filename in all_files:
+    # Process each file in the directory using tqdm
+    for filename in tqdm(all_files, desc="Creating y:"):
         if filename.endswith('.pt'):
             entry_id = filename.split('.')[0]  # Assuming the file names are just EntryID.pt
             file_path = os.path.join(input_directory, filename)
             data_obj = torch.load(file_path)
 
             # Initialize y vector for all unique terms
-            y = torch.zeros(len(unique_terms), dtype=torch.float32)
+            y = torch.zeros(len(unique_terms), dtype=torch.float16)
 
             if entry_id in df['EntryID'].values:
                 # Create a vector for the terms associated with this entry
@@ -44,4 +46,4 @@ if __name__ == "__main__":
     input_directory = "/content/drive/MyDrive/protein-DATA/sample"
     foldseek_labels = ".tsv"
     
-    foldseek_labels(input_directory, foldseek_labels)
+    foldseek_labels_regression(input_directory, foldseek_labels)
