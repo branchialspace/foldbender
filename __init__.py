0754203e18e7b7283c4e023cd72de217a4da56c6
dollabillgates <31433587+dollabillgates@users.noreply.github.com>
2024-01-22 17:37:37 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from .transform.NX_PyG import nx_pyg
 from .transform.GO_labels import go_labels
 from .transform.ESM2_labels import esm2_labels
 from .transform.foldseek_labels_regression import foldseek_labels_regression
+from .transform.foldseek_labels_multiclass import foldseek_labels_multiclass
 from .transform.alphafold_dataset import Alphafold
 from .transform.atomic_posenc import soap_local
 from .transform.eigen_posenc import precompute_eigens
