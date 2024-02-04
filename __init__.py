f4f70ba602c36d23318ac62d842ad0302d14e0bf
branchialspace <31433587+branchialspace@users.noreply.github.com>
2024-02-03 22:42:51 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,6 @@ from .transform.soap_posenc import soap_local
 from .transform.eigen_posenc import precompute_eigens
 from .transform.minmax_norm import minmax_norm
 from .transform.GO_split import go_split
-from .transform.foldseek_split import foldseek_split
 from .transform.foldseek_targets import foldseek_targets
 from .utils.delete_y import delete_y
 from .utils.to_f16 import convert_to_float16
